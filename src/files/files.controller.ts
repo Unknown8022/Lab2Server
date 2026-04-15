b9f048bdff44088535afd4d0881639c4bad3a236
Олексій Хавич <153214886+Unknown8022@users.noreply.github.com>
2026-04-15 18:31:27 +0000

    test

---
+++ src/files/files.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file')) // Ключ у Postman має бути 'file'
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.filesService.uploadFile(file.buffer, file.originalname);
+  }
+}
